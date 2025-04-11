d7dffce2db012c4c5bc9d546167efe702b198cfa
Angel M Miguel <hi@angel.kiwi>
2025-04-11 12:53:53 +0200

    feat: improve overall styling to match Endor

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -4,7 +4,22 @@ import starlightPlugin from '@astrojs/starlight-tailwind';
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				background: 'rgb(var(--c-background))',
+				borderColor: {
+					primary: 'rgb(var(--c-primary))'
+				},
+				text: 'rgb(var(--c-text))',
+				'text-light': 'rgb(var(--c-text-light))',
+				primary: 'rgb(var(--c-primary))',
+				accent: 'rgb(var(--c-accent))',
+				light: 'rgb(var(--c-light))'
+			},
+			boxShadow: {
+				neon: '0 0 3px 2px rgb(19 170 101 / 0.3)'
+			}
+		},
 	},
 	plugins: [starlightPlugin()],
 }
